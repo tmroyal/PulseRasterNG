22e00e1e8f6de3085ebbaf926d8065c7b3c7d396
Thomas Royal <thomas.m.royal@gmail.com>
2025-08-23 22:02:51 -0400

    😎

--- scripts/twirling_cube.lua
+++ scripts/twirling_cube.lua
@@ -12,34 +12,44 @@ fill(1, 1, 1, 1)
 -- schedule_with(1, DebugPrint)
 
 local last = timeSec()
+local dist1 = 0
+local dist2 = 0
 
 metro(0.125, function()
-    print("Metro Tick")
-    print("Time: " .. timeSec())
-    local freq = math.sin(timeSec()*0.1*math.pi)*200 + 400
-    synth("chirp", "note", freq, 0.5)
-    -- print("error: " .. (timeSec() - last - 0.25))
-    last = timeSec()
+    local freq1 = dist1 * 400 + 200
+    local freq2 = dist2 * 400 + 200
+    synth("chirp", "note", freq1+freq2, 0.5)
     circle(math.random(0, width), math.random(0, height), 10);
 end)
 
-local pattern = {
-    "kick", "snare", "kick", "clhh"
+local pattern1= {
+    "kick", false, "snare", false, false, "kick", "snare", false
+}
+local pattern2 ={
+    false, "clhh", false, "clhh", false, "clhh", false, "ohh"
 }
 local idx = 1
 
-metro(0.5, function()
-    print("Drum Tick")
-    synth("drumz", pattern[idx])
+metro(0.25, function()
+    local d1 = pattern1[idx]
+    local d2 = pattern2[idx]
+
+    if d1 then
+        synth("drumz", d1)
+    end
+    if d2 then
+        synth("drumz", d2)
+    end
+
     idx = idx + 1
-    if idx > #pattern then
+    if idx > #pattern1 then
         idx = 1
     end
 end)
 
 function draw()
-    local dist = math.sin(timeSec()*0.1*math.pi)
-    local dist2 = math.sin(timeSec()*0.2*math.pi)
+    dist = math.sin(timeSec()*0.1*math.pi)
+    dist2 = math.sin(timeSec()*0.2*math.pi)
     local level = math.abs(dist);
 
     -- synth_set(br, "freq", 200 + level * 400)
