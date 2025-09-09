d879c189355b359f1b3209304cfd265b233d18fb
Thomas Royal <thomas.m.royal@gmail.com>
2025-09-08 21:52:57 -0400

    gain_send (#34)

--- script_runner/ScriptRunner.cpp
+++ script_runner/ScriptRunner.cpp
@@ -45,7 +45,6 @@ void ScriptRunner::run_callback(pdArg arg){
     if (callbacks.find(arg.token) == callbacks.end()) {
         return;
     }
-
     sol::protected_function cb = callbacks[arg.token];
 
     switch (arg.type){
@@ -64,7 +63,6 @@ void ScriptRunner::run_callback(pdArg arg){
 int ScriptRunner::store_callback(sol::protected_function cb){
     int token = current_callback_token++;
     callbacks[token] = std::move(cb);
-    std::cout << token << std::endl;
     return token;
 }
 
