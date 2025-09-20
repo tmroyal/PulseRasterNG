5925274cf471d2ac31315a0ca2d7b923d1b513d6
Thomas Royal <thomas.m.royal@gmail.com>
2025-09-19 22:05:31 -0400

    add mouse event

--- controller_engine/EventProcessor.cpp
+++ controller_engine/EventProcessor.cpp
@@ -1,6 +1,7 @@
 #include "EventProcessor.hpp"
 
 void EventProcessor::process() {
+    // These will need to ve updated if raylib adds more mouse buttons
     for (int i = 0; i <= MOUSE_BUTTON_BACK; i++) {
         if (IsMouseButtonPressed(i)) {
             std::cout << "Mouse button " << i << " pressed" << std::endl;
@@ -9,4 +10,5 @@ void EventProcessor::process() {
             std::cout << "Mouse button " << i << " released" << std::endl;
         }
     }
+    // TODO: Support custom touch events
 }
