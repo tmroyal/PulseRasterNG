b97b73787a53da142f55834e8f7333127f5f3721
Thomas Royal <thomas.m.royal@gmail.com>
2025-09-19 22:15:28 -0400

    add mouseDown and up

--- controller_engine/EventProcessor.hpp
+++ controller_engine/EventProcessor.hpp
@@ -7,5 +7,10 @@
 class EventProcessor {
 public:
     void process();
+    void onMouseDown(int button, sol::protected_function handler);
+    void onMouseUp(int button, sol::protected_function handler);
 private:
+    std::unordered_map<int, std::vector<sol::protected_function>> mouseDownHandlers;
+    std::unordered_map<int, std::vector<sol::protected_function>> mouseUpHandlers;
+    void runHandlers(const std::unordered_map<int, std::vector<sol::protected_function>>& handlersMap, int button);
 };
